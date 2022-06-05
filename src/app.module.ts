9e7da81e036c7ba688431e5f8f01a8622ee3d622
vitaliimakovsky <vitaliimakovsky@MacBook-Pro-Vitalii.local>
2022-06-05 09:27:31 +0300

    edit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { FileModule } from './file/file.module';
 import { TrackModule } from './track/track.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://Vitalii:admin@cluster0.1wgh9.mongodb.net/music?retryWrites=true&w=majority'),
     TrackModule,
-    // AuthModule
+    AuthModule,
+    FileModule
   ],
   controllers: [],
   providers: [],
